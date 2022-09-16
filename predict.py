86c5713e8eb29301e96683f2f9bbf361161be27a
PMeng97 <89506540+PMeng97@users.noreply.github.com>
2022-09-16 14:12:31 -0700

    batch_size

--- predict.py
+++ predict.py
@@ -21,7 +21,7 @@ def tmp_convert_to_images(obj):
         img.append(PIL.Image.fromarray(out_array))
     return img
 
-def txt2img(prompt):
+def txt2img(prompt, batch_size):
     # git lfs install
     # git clone https://huggingface.co/osanseviero/BigGAN-deep-128
     prompt = prompt.replace('+', ' ')
@@ -32,6 +32,7 @@ def txt2img(prompt):
         pass
     else:
         ssl._create_default_https_context = _create_unverified_https_context
+
     nltk.download('wordnet')
     nltk.download('omw-1.4')
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
@@ -42,8 +43,8 @@ def txt2img(prompt):
     # Prepare a input
     truncation = 0.4
     try:
-        class_vector = one_hot_from_names([prompt], batch_size=3)
-        noise_vector = truncated_noise_sample(truncation=truncation, batch_size=3)
+        class_vector = one_hot_from_names([prompt], batch_size=batch_size)
+        noise_vector = truncated_noise_sample(truncation=truncation, batch_size=batch_size)
 
         # All in tensors
         noise_vector = torch.from_numpy(noise_vector)
@@ -57,19 +58,14 @@ def txt2img(prompt):
     else:
         model.to(device)
 
-        print("Image Generating")
+        print("Images Generating")
         # Generate an image
         with torch.no_grad():
             output = model(noise_vector, class_vector, truncation)
 
-        print("Model Generation Finished")
+        print("Images Generation Finished")
         # If you have a GPU put back on CPU
         output = output.to('cpu')
-        # image = tmp_convert_to_images(output)[0]
         images = tmp_convert_to_images(output)
-
-        # data_id = str(uuid.uuid4())
-        # img_name = data_id+'_'+('_').join(prompt.split())
-        # # Modification needed for MongoDB
-        # image.save(img_name+".png")
         return images
+
